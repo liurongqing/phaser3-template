fdeb8d78b33257da723e4459e3523a1219e13b54
liurongqing <phpgege@gmail.com>
2019-12-01 06:15:46 -0800

    1

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,28 @@
-import { OPTION_GAME } from '@/const'
 import * as scenes from '@/scenes'
-import { os } from '@/utils'
 
 const scene = []
 for (let i in scenes) {
   scene.push(scenes[i])
 }
 
-const scale: any = {
-  parent: OPTION_GAME.parent,
-  autoCenter: Phaser.Scale.CENTER_BOTH,
-  mode: OPTION_GAME.mode,
-  width: OPTION_GAME.width,
-  height: OPTION_GAME.height
-}
-
-if (os.isPc) {
-  scale.max = {
-    width: 375,
-    height: 667
-  }
-}
 const config: any = {
   type: Phaser.AUTO,
   backgroundColor: 0x000000,
-  scale,
+  scale: {
+    mode: Phaser.Scale.ENVELOP,
+    parent: 'app',
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: 750,
+    height: 1334,
+    min: {
+      width: 750,
+      height: 1334
+    },
+    max: {
+      width: 750,
+      height: 1334
+    }
+  },
   scene
 }
 
