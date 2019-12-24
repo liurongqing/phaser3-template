9948d46234da02847693397918eb3701b903a68e
liurongqing <phpgege@gmail.com>
2019-12-24 16:31:17 +0800

    添加默认场景

--- src/index.ts
+++ src/index.ts
@@ -8,20 +8,12 @@ for (let i in scenes) {
 const config: any = {
   type: Phaser.AUTO,
   backgroundColor: 0x000000,
+  parent: null,
   scale: {
-    mode: Phaser.Scale.ENVELOP,
-    parent: 'app',
+    mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
     width: 750,
-    height: 1334,
-    min: {
-      width: 750,
-      height: 1334
-    },
-    max: {
-      width: 750,
-      height: 1334
-    }
+    height: 1334
   },
   scene
 }
