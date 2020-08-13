0c37406d493f1be028089321fbf0499294bcd68b
liurongqing <phpgege@gmail.com>
2020-08-13 16:06:48 +0800

    线上测试

--- docs/game.js
+++ docs/game.js
@@ -104,7 +104,7 @@ class MainScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.setBaseURL('http://labs.phaser.io');
+    this.load.setBaseURL('https://labs.phaser.io');
     this.load.image('sky', 'assets/skies/space3.png');
     this.load.image('logo', 'assets/sprites/phaser3-logo.png');
     this.load.image('red', 'assets/particles/red.png'); // this.load.setBaseURL(BASE_URL)
--- src/scenes/Main/index.ts
+++ src/scenes/Main/index.ts
@@ -5,7 +5,7 @@ export class MainScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.setBaseURL('http://labs.phaser.io')
+    this.load.setBaseURL('https://labs.phaser.io')
     this.load.image('sky', 'assets/skies/space3.png')
     this.load.image('logo', 'assets/sprites/phaser3-logo.png')
     this.load.image('red', 'assets/particles/red.png')
