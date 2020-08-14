7d7430a70b5b15005d21ba46f321dcc91733541d
liurongqing <phpgege@gmail.com>
2020-08-14 12:40:03 +0800

    修复es6未转成es5问题

--- src/scenes/main/index.ts
+++ src/scenes/main/index.ts
@@ -1,4 +1,3 @@
-// import { BASE_URL, PATH_URL } from '@/const'
 export class MainScene extends Phaser.Scene {
   constructor() {
     super('MainScene')
@@ -9,10 +8,6 @@ export class MainScene extends Phaser.Scene {
     this.load.image('sky', 'assets/skies/space3.png')
     this.load.image('logo', 'assets/sprites/phaser3-logo.png')
     this.load.image('red', 'assets/particles/red.png')
-    // this.load.setBaseURL(BASE_URL)
-    // this.load.setPath(PATH_URL)
-    // this.load.image('bg', 'images/bg.jpg')
-    // console.log('git submodule test')
   }
 
   create() {
@@ -30,7 +25,5 @@ export class MainScene extends Phaser.Scene {
     logo.setCollideWorldBounds(true);
 
     emitter.startFollow(logo);
-
-    // this.add.image(0, 0, 'bg').setOrigin(0, 0)
   }
 }
